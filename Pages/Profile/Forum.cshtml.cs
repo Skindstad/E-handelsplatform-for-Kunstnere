551f6c9d6c53b1d6c84325f856ce3fa3d2b45d17
Skindstad <39044695+Skindstad@users.noreply.github.com>
2024-10-01 11:10:18 +0200

    skab en header for profiler

---
+++ Pages/Profile/Forum.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_handelsplatform_for_Kunstnere.Pages.Profile
+{
+    public class ForumModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

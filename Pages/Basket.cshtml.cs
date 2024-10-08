2cf6644688ea1a9f2044988193dc61f11ee8a66e
Jakob Skindstad Frederiksen <jakobsfrederiksen1@gmail.com>
2024-10-08 13:42:38 +0200

    Kruv

---
+++ Pages/Basket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EhandelsplatformforKunstnere.Pages
+{
+    public class BasketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
